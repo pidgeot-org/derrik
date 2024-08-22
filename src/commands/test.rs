4d5c8c0199a9baf2ce272d1e64e3afc34c637ad4
Massimiliano Pippi <mpippi@gmail.com>
2024-08-22 09:16:40 +0200

    add color support

--- src/commands/test.rs
+++ src/commands/test.rs
@@ -13,8 +13,12 @@ impl Cli {
         trace!("trace {}", self.text);
         debug!("debug {}", self.text);
         info!("info {}", self.text);
+        success!("success {}", self.text);
+        waiting!("waiting {}", self.text);
         warn!("warn {}", self.text);
         error!("error {}", self.text);
+        display!("display {}", self.text);
+        critical!("critical {}", self.text);
 
         Ok(())
     }
--- src/macros.rs
+++ src/macros.rs
@@ -1,17 +1,46 @@
-macro_rules! define_display_macro {
-    ($name:ident, $level:ident, $d:tt) => {
-        macro_rules! $name {
-            ($d($d arg:tt)*) => {
-                if log::Level::$level <= *$crate::app::verbosity() {
-                    eprintln!($d($d arg)*);
-                }
-            };
-        }
-    };
+macro_rules! display {
+    ($($arg:tt)*) => {{
+        use owo_colors::OwoColorize;
+        println!(
+            "{}",
+            format!($($arg)*)
+                .if_supports_color(owo_colors::Stream::Stdout, |text| text.bold())
+        );
+    }};
 }
 
-define_display_macro!(trace, Trace, $);
-define_display_macro!(debug, Debug, $);
-define_display_macro!(info, Info, $);
-define_display_macro!(warn, Warn, $);
-define_display_macro!(error, Error, $);
+macro_rules! critical {
+    ($($arg:tt)*) => {{
+        use owo_colors::OwoColorize;
+        eprintln!(
+            "{}",
+            format!($($arg)*)
+                .if_supports_color(owo_colors::Stream::Stderr, |text| text.bright_red())
+        );
+    }};
+}
+
+macro_rules! define_display_macro {
+    ($name:ident, $level:ident, $style:ident, $d:tt) => (
+        macro_rules! $name {
+            ($d($d arg:tt)*) => {{
+                use owo_colors::OwoColorize;
+                if log::Level::$level <= *$crate::app::verbosity() {
+                    eprintln!(
+                        "{}",
+                        format!($d($d arg)*)
+                            .if_supports_color(owo_colors::Stream::Stderr, |text| text.$style())
+                    );
+                }
+            }};
+        }
+    );
+}
+
+define_display_macro!(trace, Trace, underline, $);
+define_display_macro!(debug, Debug, italic, $);
+define_display_macro!(info, Info, bold, $);
+define_display_macro!(success, Info, bright_cyan, $);
+define_display_macro!(waiting, Info, bright_magenta, $);
+define_display_macro!(warn, Warn, bright_yellow, $);
+define_display_macro!(error, Error, bright_red, $);

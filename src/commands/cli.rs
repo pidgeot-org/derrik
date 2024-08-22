0dad3f9c3460605f4e24ee8ec7b938f2adaf4dd7
Massimiliano Pippi <mpippi@gmail.com>
2024-08-22 09:02:41 +0200

    add run command

--- src/commands/cli.rs
+++ src/commands/cli.rs
@@ -1,15 +1,23 @@
 use anyhow::Result;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 /// Derrik helps you move around data with confidence
 #[derive(Parser, Debug)]
 #[command(version, bin_name = "derrik", disable_help_subcommand = true)]
-pub struct Cli {}
+pub struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Run(super::run::Cli),
+}
 
 impl Cli {
     pub fn exec(&self) -> Result<()> {
-        println!("Hello, World!");
-
-        Ok(())
+        match &self.command {
+            Commands::Run(cli) => cli.run(),
+        }
     }
 }

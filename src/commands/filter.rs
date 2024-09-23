e5525d85e80158698da231ebcb3be6957a1cdca4
Massimiliano Pippi <mpippi@gmail.com>
2024-09-24 00:01:17 +0200

    more options

--- src/commands/filter.rs
+++ src/commands/filter.rs
@@ -1,8 +1,13 @@
 use anyhow::Result;
-use clap::Args;
+use clap::{Args, ValueEnum};
 use std::process::exit;
 use std::fs;
 
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
+enum Operator {
+    Contains,
+    Icontains,
+}
 
 /// Print file content
 ///
@@ -11,7 +16,17 @@ use std::fs;
 #[command()]
 pub struct Cli {
     #[arg(required = true, trailing_var_arg = true, allow_hyphen_values = true)]
-    args: Vec<String>,
+    input_files: Vec<String>,
+
+    /// The name of the field that will be use for filtering.
+    #[arg(long)]
+    text_key: Option<String>,
+
+    #[arg(long, value_enum)]
+    operator: Option<Operator>,
+
+    // #[arg(long)]
+    // matches: Vec<String>,
 }
 
 impl Cli {
@@ -19,7 +34,7 @@ impl Cli {
         // Get the file.txt path
         // Check if the file.txt exists
         // Print the content
-        let contents = fs::read_to_string(&self.args[0])
+        let contents = fs::read_to_string(&self.input_files[0])
             .expect("The file path doesn't exist");
         println!("With text:\n{contents}");
 
